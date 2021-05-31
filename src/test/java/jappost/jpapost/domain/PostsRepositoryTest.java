88755f3825b45bc5eb7e88055373dd5bf76b348e
JunyoungDev <wnsdud2336@naver.com>
2021-05-31 23:21:18 +0900

    Posts 도메인 테스트

---
+++ src/test/java/jappost/jpapost/domain/PostsRepositoryTest.java
@@ -0,0 +1,41 @@
+package jappost.jpapost.domain;
+
+import jappost.jpapost.domain.posts.Posts;
+import jappost.jpapost.domain.posts.PostsRepository;
+import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.transaction.annotation.Transactional;
+
+import java.util.List;
+
+import static org.assertj.core.api.Assertions.*;
+
+@SpringBootTest
+@Transactional
+class PostsRepositoryTest {
+    @Autowired
+    PostsRepository postsRepository;
+
+    @Test
+    public void 게시글저장_불러오기() throws Exception {
+        //given
+        String title = "테스트 제목";
+        String content = "테스트 본문";
+
+        postsRepository.save(Posts.builder()
+        .title(title)
+        .content(content)
+        .author("wnsdud2336@naver.com")
+        .build());
+
+        //when
+        List<Posts> postsList = postsRepository.findAll();
+
+        //then
+        Posts posts = postsList.get(0);
+        assertThat(posts.getTitle()).isEqualTo(title);
+        assertThat(posts.getContent()).isEqualTo(content);
+        assertThat(posts.getAuthor()).isEqualTo("wnsdud2336@naver.com");
+    }
+}
