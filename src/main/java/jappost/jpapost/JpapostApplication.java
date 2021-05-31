8063ac8967f37771f270312ddf0310c2574cb086
JunyoungDev <wnsdud2336@naver.com>
2021-05-31 23:00:57 +0900

    Initial commit

---
+++ src/main/java/jappost/jpapost/JpapostApplication.java
@@ -0,0 +1,13 @@
+package jappost.jpapost;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class JpapostApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(JpapostApplication.class, args);
+	}
+
+}
